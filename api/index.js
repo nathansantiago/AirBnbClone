4d9f5f66854a9823094acd532aada5b9ebf0cfa1
Nathan Santiago <santiagonathan9254@gmail.com>
2024-06-11 23:27:35 -0400

    Create registration api. Connect to mongodb.

--- api/index.js
+++ api/index.js
@@ -1,8 +1,36 @@
 const express = require('express');
+const cors = require('cors');
+const { default: mongoose } = require('mongoose');
+const bcrypt = require('bcryptjs');
+const User = require('./models/User.js');
+require('dotenv').config();
 const app = express();
 
+const bcryptSalt = bcrypt.genSaltSync(12);
+
+app.use(express.json());
+
+app.use(cors({
+    credentials: true,
+    origin: 'http://localhost:5173',
+}));
+
+mongoose.connect(process.env.ATLAS_URI);
+
+// cors allows api and client to communicate on different ports
 app.get('/test', (req,res) => {
     res.json('test ok');
+});
+
+app.post('/register', async (req, res) => {
+    const {name, email, password} = req.body;
+    const userDoc = await User.create({
+        name,
+        email,
+        password:bcrypt.hashSync(password, bcryptSalt), // Want to encrypt passwords first
+    });
+
+    res.json(userDoc);
 });
 
 app.listen(4000);
